5d4f25402030433a526845f6720d3b7712b7c47e
Matteo Perona <matteowork@Matteos-MacBook-Air.local>
2023-09-14 16:47:55 -0700

    added a contact form

--- src/views/inner-pages/portfolio/WorksGrid.jsx
+++ src/views/inner-pages/portfolio/WorksGrid.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Helmet } from "react-helmet";
-import HeaderDefault from "../../../components/header/HeaderDefault";
+import HeaderDefault from "../../../components/header/HeaderHomeDefault";
 import CopyRight from "../../../components/footer/copyright/CopyRight";
 import Footer from "../../../components/footer/Footer";
 import PortfolioSix from "../../../components/portfolio/PortfolioSix";
@@ -13,7 +13,7 @@ const WorksGrid = () => {
         <title>Moonex - Works/Portfolio Grid</title>
       </Helmet>
       {/* End Page SEO Content */}
-      <div className="ptf-site-wrapper__inner">
+      <div className="ptf-site-wrapper__inner margin_bottom_30">
         <HeaderDefault />
         {/* End  HeaderHomeDefault */}
 
@@ -35,101 +35,65 @@ const WorksGrid = () => {
                       data-aos="fade"
                       data-aos-delay="0"
                     >
-                      <h1 className="large-heading">Portfolio</h1>
+                      <div className="container-y">
+
+                        <div>
+                          <h2 className="h1 large-heading">Contact Us</h2>
+                          <h6 className="subheading">COMPASS@COMPASS.ORG</h6>
+                        </div>
+
+                        <div className='container-avg'>
+
+                          <form className="container-inputs">
+
+                            <div class="form-group">
+                              <label for="exampleInputEmail1">Name *</label>
+                              <div className="container-name-input">
+                                <input type="name" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="First" />
+                                <input type="name" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Last" />
+                              </div>
+                            </div>
+                            <div class="form-group">
+                              <label for="exampleInputEmail1">Email address</label>
+                              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+                              <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                            </div>
+
+                            <div class="form-group">
+                              <label for="exampleFormControlTextarea1">Text *</label>
+                              <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="Just expressing interest; have a project in mind; or any comments/questions, etc."></textarea>
+                            </div>
+
+                            <button type="submit" class="btn btn-secondary">Submit</button>
+                          </form>
+
+                          <div className="container-socials">
+                            <div>
+                              <h5>Instagram</h5>
+                              <small>@compassinstitutionucsd</small>
+                            </div>
+                            <div>
+                              <h5>Facebook</h5>
+                              <small>@compassinstitutionucsd</small>
+                            </div>
+                            <div>
+                              <h5>LinkedIn</h5>
+                              <small>@compassinstitutionucsd</small>
+                            </div>
+                          </div>
+
+                        </div>
+
+                      </div>
+
                     </div>
                   </div>
                 </div>
-                {/* <!--Spacer--> */}
-                <div
-                  className="ptf-spacer"
-                  style={{ "--ptf-xxl": "4.375rem" }}
-                ></div>
               </div>
-              {/* End .container-xxl */}
-
-              {/*=============================================
-                Start Portfolio main 
-                ============================================== */}
-              <div className="container-xxl">
-                <PortfolioSix />
-                {/* <!--Spacer--> */}
-                <div
-                  className="ptf-spacer"
-                  style={{ "--ptf-xxl": "8.125rem", "--ptf-md": "4.0625rem" }}
-                ></div>
-              </div>
-            </section>
-
-            <section>
-              <div className="container-xxl">
-                {/* <!--Divider--> */}
-                <div
-                  className="ptf-divider"
-                  style={{
-                    "--ptf-height": "1px",
-                    "--ptf-color": "var(--ptf-color-14)",
-                  }}
-                ></div>
-              </div>
-            </section>
-
-            {/*=============================================
-            Start Portfolio More 
-            ============================================== */}
-            <section>
-              {/* <!--Spacer--> */}
-              <div
-                className="ptf-spacer"
-                style={{ "--ptf-xxl": "8.75rem", "--ptf-md": "4.0625rem" }}
-              ></div>
-
-              <div className="container-xxl">
-                {/* <!--Animated Block--> */}
-                <div
-                  className="ptf-animated-block"
-                  data-aos="fade"
-                  data-aos-delay="0"
-                >
-                  <div
-                    className="ptf-isotope-grid row"
-                    style={{
-                      "--bs-gutter-x": "4rem",
-                      "--bs-gutter-y": "5.75rem",
-                    }}
-                  >
-                    <RelatedPortfolio />
-                  </div>
-                </div>
-                {/* <!--Spacer--> */}
-                <div
-                  className="ptf-spacer"
-                  style={{ "--ptf-xxl": "10rem", "--ptf-md": "5rem" }}
-                ></div>
-                <div className="text-center">
-                  {/* <!--Animated Block--> */}
-                  <div
-                    className="ptf-animated-block"
-                    data-aos="fade"
-                    data-aos-delay="0"
-                  >
-                    <a className="ptf-load-more" href="#">
-                      More
-                    </a>
-                  </div>
-                </div>
-              </div>
-
-              {/* <!--Spacer--> */}
-              <div
-                className="ptf-spacer"
-                style={{ "--ptf-xxl": "10rem", "--ptf-md": "5rem" }}
-              ></div>
             </section>
           </div>
-          {/* End .ptf-page */}
         </div>
       </div>
-      {/* End .main */}
 
       {/* <!--Footer--> */}
       <footer className="ptf-footer ptf-footer--style-1">
